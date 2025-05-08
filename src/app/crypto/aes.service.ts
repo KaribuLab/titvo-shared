a73cf59cf7618e9011ff99ef42192558ef12d2fe
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-08 04:15:33 -0400

    feat: Inyectar el nombre de la propiedad de la clave de cifrado en el servicio AES y ajustar la obtención del secreto

--- src/app/crypto/aes.service.ts
+++ src/app/crypto/aes.service.ts
@@ -1,12 +1,14 @@
 import { createCipheriv, CipherKey } from 'crypto'
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { SecretService } from '@titvo/shared'
+
+export const ENCRYPTION_KEY_NAME_PROPERTY = 'ENCRYPTION_KEY_NAME'
 @Injectable()
 export class AesService {
-  constructor (private readonly secretService: SecretService) { }
+  constructor (private readonly secretService: SecretService, @Inject(ENCRYPTION_KEY_NAME_PROPERTY) private readonly encryptionKeyName: string) { }
 
   async encrypt (data: string): Promise<string> {
-    const secretBase64 = await this.secretService.get('aes_secret')
+    const secretBase64 = await this.secretService.get(this.encryptionKeyName)
     if (secretBase64 === undefined) {
       throw new Error('AES secret not found')
     }
