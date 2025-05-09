08734388556d745914f5966cb86023aee632b52b
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-09 12:09:00 -0400

    chore: Agregar exportación de storage.service en index.ts

---
+++ src/core/outputs/storage.service.ts
@@ -0,0 +1,15 @@
+export interface GetSignedUrlInput {
+  containerName: string
+  inputPath: string
+  filePath: string
+  contentType: string
+  expiresIn: number
+}
+
+export interface GetSignedUrlOutput {
+  url: string
+}
+
+export abstract class StorageService {
+  abstract getSignedUrl (input: GetSignedUrlInput): Promise<GetSignedUrlOutput>
+}
