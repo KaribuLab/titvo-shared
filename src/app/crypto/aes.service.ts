948174d3dea29c5bde83cfbc42a8aceb4f37a61e
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-05-12 19:37:34 -0400

    feat: Agregar método de desencriptación en el servicio AES

--- src/app/crypto/aes.service.ts
+++ src/app/crypto/aes.service.ts
@@ -1,4 +1,4 @@
-import { createCipheriv, CipherKey } from 'crypto'
+import { createCipheriv, createDecipheriv, CipherKey } from 'crypto'
 import { Inject, Injectable } from '@nestjs/common'
 import { SecretService } from '@titvo/shared'
 
@@ -15,5 +15,15 @@ export class AesService {
     const secret = Buffer.from(secretBase64, 'base64') as CipherKey
     const cipher = createCipheriv('aes-256-ecb', secret, null)
     return cipher.update(data, 'utf8', 'base64') + cipher.final('base64')
+  }
+
+  async decrypt (data: string): Promise<string> {
+    const secretBase64 = await this.secretService.get(this.encryptionKeyName)
+    if (secretBase64 === undefined) {
+      throw new Error('AES secret not found')
+    }
+    const secret = Buffer.from(secretBase64, 'base64') as CipherKey
+    const decipher = createDecipheriv('aes-256-ecb', secret, null)
+    return decipher.update(data, 'base64', 'utf8') + decipher.final('utf8')
   }
 }
